f878c528a889a808cc6615abc4d4f97fd4592b8d
masonkm <kmason1379@gmail.com>
2020-06-27 17:43:50 -0400

    Started caesar cipher

---
+++ caesar.cpp
@@ -0,0 +1,16 @@
+// Author: Kenneth Mason
+#include <iostream>
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+  // Takes a numerical key and a string to be encoded as input. A negative key denotes a left-shift
+  if(argc != 3){
+    cout << "Expected 2 arguments: an integer key and an input string" << endl;
+    return 1;
+  }
+
+
+  return 0;
+}
