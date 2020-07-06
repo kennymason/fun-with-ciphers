43867fe793e79c0bc8ee81e1549f771095751f3b
masonkm <kmason1379@gmail.com>
2020-07-05 21:51:03 -0400

    Finished English to Pig Latin translator

--- piglatin.cpp
+++ piglatin.cpp
@@ -1,20 +1,48 @@
 // Author: Kenneth Mason
 // Pig Latin
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <limits>
+#include <sstream>
 using namespace std;
 
-bool alpha = true;
+bool is_vowel(char c) {
+  return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+}
 
 string translate(string word){
   // RULES:
   // Words that begin with consonants: all letters before initial vowel are placed at the end, then "ay" is added
   // Words that begin with vowels: "yay" is added to end of word
   // Hyphenated words should be split up and translated separately
-  
-
-
+  if (word.find('-') != string::npos){
+    char *wrd = &word[0];
+    char *token = strtok(wrd, "-");
+    string ret;
+    // Keep printing tokens while one of the
+    // delimiters present in str[].
+    while (token != NULL) {
+      ret += (translate(token) + "-");
+      token = strtok(NULL, "-");
+    }
+    ret.pop_back();
+    return ret;
+  } else {
+    char f = tolower(word.front());
+    for (int i = 0; i < word.length(); i++){
+      if (is_vowel(tolower(word[i]))){
+        if (i == 0){
+          // First character of the word is a vowel
+          word += "yay";
+          return word;
+        }
+        // First character of the word is a consonant
+        // returns a string of all letters from the first vowel to the end of the word, then the letters up to the first vowel, then "ay"
+        return word.substr(i, word.length() - 1) + word.substr(0, i) + "ay";
+      }
+    }
+    return word + "ay";
+  }
 }
 
 int main() {
@@ -27,15 +55,15 @@ int main() {
     cout << "English: ";
     getline(cin, str);
     istringstream ss(str);
-    cout << "Pig Latin: "
+    cout << "Pig Latin: ";
 
-    do {
+    while (ss) {
       string word;
       ss >> word;
-      cout << translate(word) << " ";
-    } while (ss);
+      if(!word.empty()) cout << translate(word) << " ";
+    }
 
-    cout << "Again? (y/n): ";
+    cout << endl << "Again? (y/n): ";
     cin >> c;
     char a = tolower(c[0]);
     if(a == 'y'){
